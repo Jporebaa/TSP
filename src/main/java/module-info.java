56f2371dca8aab5d325f9f44626f7ec172684815
jakub <jakubporeba8@gmail.com>
2024-03-20 21:07:10 +0100

    Create Login Page

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.tspsystem {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires javafx.graphics;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
@@ -12,4 +13,7 @@ module com.example.tspsystem {
 
     opens com.example.tspsystem to javafx.fxml;
     exports com.example.tspsystem;
+    exports com.example.tspsystem.controllers;
+    opens com.example.tspsystem.controllers to javafx.fxml;
+
 }
