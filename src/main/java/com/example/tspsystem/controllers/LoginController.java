0ce33b5a2d737e321ddde05b48dcf2d23b8a9108
dominik228820 <dominik228820@gmail.com>
2024-04-23 12:42:14 +0200

    Sprint 4 Działające logowanie

--- src/main/java/com/example/tspsystem/controllers/LoginController.java
+++ src/main/java/com/example/tspsystem/controllers/LoginController.java
@@ -1,12 +1,14 @@
 package com.example.tspsystem.controllers;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.event.ActionEvent;
 import javafx.stage.Stage;
 
@@ -15,7 +17,9 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-
+import java.util.Map;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 public class LoginController {
 
     @FXML
@@ -24,8 +28,33 @@ public class LoginController {
     @FXML
     private PasswordField passwordField;
 
-    @FXML
-    private PasswordField languageField;
+    private static class SessionManager {
+        private static UserSession currentUserSession;
+
+        public static UserSession getCurrentUserSession() {
+            return currentUserSession;
+        }
+
+        public static void setCurrentUserSession(UserSession userSession) {
+            SessionManager.currentUserSession = userSession;
+        }
+
+        public static void clearSession() {
+            currentUserSession = null;
+        }
+    }
+
+    public static class UserSession {
+        private Integer userId;
+
+        public UserSession(Integer userId) {
+            this.userId = userId;
+        }
+
+        public Integer getUserId() {
+            return userId;
+        }
+    }
 
     @FXML
     private void handleLoginButtonAction(ActionEvent event) {
@@ -45,34 +74,46 @@ public class LoginController {
         client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
                 .thenApply(HttpResponse::body)
                 .thenAccept(response -> {
-                    if (response.contains("Użytkownik zalogowany")) {
-                        redirectToHome(); // Przekierowanie do widoku głównego.
+                    // Przekształcanie odpowiedzi JSON na mapę
+                    Map<String,Object> responseMap = new Gson().fromJson(response, new TypeToken<Map<String,Object>>(){}.getType());
+
+                    if (!responseMap.containsKey("error")) {
+                        // Pobranie userId z odpowiedzi i ustawienie sesji
+                        Number userId = (Number) responseMap.get("userId");
+                        SessionManager.setCurrentUserSession(new UserSession(userId.intValue()));
+                        redirectToHome();
                     } else {
-                        // Wyświetlenie komunikatu o błędzie logowania w konsoli.
-                        System.out.println("Błąd logowania: " + response);
+                        Platform.runLater(() -> showErrorMessage("Nieprawidłowy login lub hasło."));
                     }
                 })
                 .exceptionally(e -> {
-                    // Wyświetlenie stosu wywołań błędu w konsoli, jeśli żądanie się nie powiedzie.
                     e.printStackTrace();
+                    Platform.runLater(() -> showErrorMessage("Błąd połączenia z serwerem."));
                     return null;
                 });
     }
+
     private void redirectToHome() {
-        try {
-            Parent homeView = FXMLLoader.load(getClass().getResource("/com/example/tspsystem/home.fxml"));
-            Scene homeScene = new Scene(homeView);
-            Stage window = (Stage) usernameField.getScene().getWindow();
-            window.setScene(homeScene);
-            window.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Platform.runLater(() -> {
+            try {
+
+                Parent homeView = FXMLLoader.load(getClass().getResource("/com/example/tspsystem/home.fxml"));
+                Scene homeScene = new Scene(homeView);
+
+
+                Stage window = (Stage) usernameField.getScene().getWindow();
+                window.setScene(homeScene);
+                window.show();
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+        });
     }
+
     @FXML
     private void handleRegisterButtonAction(ActionEvent event) {
         try {
-
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/tspsystem/register-view.fxml"));
             Parent registerView = fxmlLoader.load();
 
@@ -82,5 +123,13 @@ public class LoginController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void showErrorMessage(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Błąd");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 }
