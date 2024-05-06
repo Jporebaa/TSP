482aa549fbf1b945fabb8aada3cfb0b16338663c
Dziki00 <bartosz.dziki3@gmail.com>
2024-05-06 20:41:59 +0200

    Poprawa przejścia między layoutami

--- src/main/java/com/example/tspsystem/controllers/HomeController.java
+++ src/main/java/com/example/tspsystem/controllers/HomeController.java
@@ -18,48 +18,21 @@ import java.sql.Statement;
 
 public class HomeController {
     @FXML
-    private ListView<String> userList;  // Referencja do ListView użytkowników.
-    @FXML
     private ListView<String> groupList; // Referencja do ListView grup.
 
-    // Lista obserwowalna przechowująca dane użytkowników i grup.
-    private ObservableList<String> userData = FXCollections.observableArrayList();
+    // Lista obserwowalna przechowująca dane grup.
     private ObservableList<String> groupData = FXCollections.observableArrayList();
 
     @FXML
     public void initialize() {
-        loadUserData();
         loadGroupData();
-        setupUserListClickListener();
         setupGroupListClickListener(); // Ustawienie obsługi kliknięć dla listy grup
-    }
-
-    private void loadUserData() {
-        String url = "jdbc:postgresql://localhost:5432/TSP";
-        String user = "postgres";
-        String password = "12345";
-        try {
-            Connection conn = DriverManager.getConnection(url, user, password);
-            Statement stmt = conn.createStatement();
-            String query = "SELECT login FROM users";
-            ResultSet rs = stmt.executeQuery(query);
-            userData.clear();
-            while (rs.next()) {
-                userData.add(rs.getString("login"));
-            }
-            userList.setItems(userData);
-            rs.close();
-            stmt.close();
-            conn.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     private void loadGroupData() {
         String url = "jdbc:postgresql://localhost:5432/TSP";
         String user = "postgres";
-        String password = "12345";
+        String password = "jesthaslo123";
         try {
             Connection conn = DriverManager.getConnection(url, user, password);
             Statement stmt = conn.createStatement();
@@ -78,17 +51,6 @@ public class HomeController {
         }
     }
 
-    private void setupUserListClickListener() {
-        userList.setOnMouseClicked((MouseEvent click) -> {
-            if (click.getClickCount() == 2) {
-                String selectedUser = userList.getSelectionModel().getSelectedItem();
-                if (selectedUser != null) {
-                    switchToChatView(selectedUser);
-                }
-            }
-        });
-    }
-
     private void setupGroupListClickListener() {
         groupList.setOnMouseClicked((MouseEvent click) -> {
             if (click.getClickCount() == 2) {
@@ -98,21 +60,6 @@ public class HomeController {
                 }
             }
         });
-    }
-
-    private void switchToChatView(String username) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/tspsystem/chat_layout.fxml"));
-            Parent chatView = loader.load();
-            ChatController chatController = loader.getController();
-            chatController.initData(username);
-            Scene chatScene = new Scene(chatView);
-            Stage window = (Stage) userList.getScene().getWindow();
-            window.setScene(chatScene);
-            window.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     private void switchToGroupChatView(String groupName) {
