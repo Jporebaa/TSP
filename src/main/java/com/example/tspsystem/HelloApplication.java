bdf5c467932d7c8ed0a1dce6cce697106c6a1caa
dominik228820 <dominik228820@gmail.com>
2024-05-07 18:29:11 +0200

    test

--- src/main/java/com/example/tspsystem/HelloApplication.java
+++ src/main/java/com/example/tspsystem/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));//test
         Scene scene = new Scene(fxmlLoader.load(), 1024, 500);
         stage.setTitle("TSP-APP!");
         stage.setScene(scene);
